5e24c8d1fda83f913dd6d9074b503c477ff4bfc3
Jasenka85 <132337552+Jasenka85@users.noreply.github.com>
2023-08-30 15:13:00 +0200

    WebAPI-pocetak
    Vjezbanje GET rute

---
+++ EdunovaWebAPI/HelloWorld/Controllers/CiklicnaMatrica.cs
@@ -0,0 +1,75 @@
+﻿namespace HelloWorld.Controllers
+{
+    public class CiklicnaMatrica
+    {
+
+        public static string[] NapraviMatricu(int redaka, int stupaca)
+        {
+
+
+            int[,] matrica = new int[redaka, stupaca];
+
+            int b = 1;
+
+            int n = 0;          
+            
+            var nizRedaka = new string[redaka];
+
+
+
+            while (b < stupaca * redaka)
+            {
+
+                for (int i = n + 1; i <= stupaca - n; i++)      // s desna na lijevo
+                {
+                    if (b <= stupaca * redaka)
+                        matrica[redaka - n - 1, stupaca - i] = b++;
+                    else break;
+                }
+
+                for (int i = redaka - n - 2; i >= n; i--)          // gore
+                {
+                    if (b <= stupaca * redaka)
+                        matrica[i, n] = b++;
+                    else break;
+                }
+
+
+                for (int i = n + 1; i <= stupaca - n - 1; i++)    // s lijeva na desno
+                {
+                    if (b <= stupaca * redaka)
+                        matrica[n, i] = b++;
+                    else break;
+                }
+
+
+                for (int i = n + 1; i <= redaka - n - 2; i++)       // dolje
+                {
+                    if (b <= stupaca * redaka)
+                        matrica[i, stupaca - n - 1] = b++;
+                    else break;
+                }
+
+                n++;
+
+            }
+
+            for (int i = 0; i < redaka; i++)
+            {
+                string s = "";
+                for (int j = 0; j < stupaca; j++)
+                { 
+                    string t = "     " + matrica[i, j];
+                    t = t[^5..];
+                    s = s + t;
+                }
+                nizRedaka[i] = s;
+            }
+
+            return nizRedaka;
+
+        }
+
+
+    }
+}
---
+++ EdunovaWebAPI/HelloWorld/Controllers/HelloWorldController.cs
@@ -0,0 +1,102 @@
+﻿
+using System.Collections.Specialized;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello world";
+        }
+
+        [HttpGet]
+        [Route("pozdrav")]
+
+        public string DrugaMetoda()
+        {
+            return "Pozdrav svijetu";
+        }
+
+        [HttpGet]
+        [Route("pozdravParametar")]
+
+        public string DrugaMetoda(string s)
+        {
+            return "Hello " + s;
+        }
+
+        [HttpGet]
+        [Route("pozdravViseParametara")]
+
+        public string DrugaMetoda(string s="", int i=0)
+        {
+            return "Hello " + s + " " + i;
+        }
+
+        // Kreirajte rutu /HelloWorld/zad1
+        // koja ne prima parametre i vraća vaše ime
+
+        [HttpGet]
+        [Route("zad1")]
+        public string MojeIme()
+        {
+            return "Jasenka Augustinović";
+        }
+
+
+        //Kreirajte rutu /HelloWorld/zad2
+        //Koja prima dva broja i vraća njihov zbroj
+
+
+        [HttpGet]
+        [Route("zad2")]
+        public int Zbroj(int b1, int b2)
+        {
+            return b1+b2;
+        }
+
+
+        //Kreirajte rutu /HelloWorld/zad3
+        //koja prima parametar brojPonavljanja
+        //Ruta vraća niz znakova "Osijek" koji ima
+        //onoliko elemenata koliko smo primili u brojPonavljanja
+
+
+        [HttpGet]
+        [Route("zad3")]
+
+        public string[] VolimOsijek(int brojPonavljanja)
+        {
+            var nizOsijeka = new string[brojPonavljanja];
+
+            for (int i=0; i<brojPonavljanja; i++)
+            {
+                nizOsijeka[i] = "Osijek";
+            }
+
+            return nizOsijeka;
+        }
+
+
+        //DZ kreirati metodu /HelloWorld/ciklicna
+        //koja prima dva parametra (x i y) a vraca
+        //ciklicnu matricu kao dvodimenzionalni niz brojeva
+
+
+        [HttpGet]
+        [Route("ciklicna")]
+
+        public string[] IspisiCiklicnuMatricu(int redaka, int stupaca)
+        {
+            return CiklicnaMatrica.NapraviMatricu(redaka, stupaca);
+        
+        }
+
+
+    }
+}
