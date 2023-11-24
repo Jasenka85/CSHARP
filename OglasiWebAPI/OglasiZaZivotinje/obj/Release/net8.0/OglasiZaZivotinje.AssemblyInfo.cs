9fe0fb3e4d49a6a6c187e5441d9be3233f8237b0
Jasenka85 <132337552+Jasenka85@users.noreply.github.com>
2023-11-24 23:36:29 +0100

    Web API - dvadeset i treci korak
    Dodana autorizacija i na frontendu.

--- OglasiWebAPI/OglasiZaZivotinje/obj/Release/net8.0/OglasiZaZivotinje.AssemblyInfo.cs
+++ OglasiWebAPI/OglasiZaZivotinje/obj/Release/net8.0/OglasiZaZivotinje.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OglasiZaZivotinje")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be86693ec0832b66591418359ed0e71db223267a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eccc85a52e22b12e53532bb6e6114aa70363102")]
 [assembly: System.Reflection.AssemblyProductAttribute("OglasiZaZivotinje")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OglasiZaZivotinje")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
